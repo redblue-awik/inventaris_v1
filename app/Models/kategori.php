66668e1bb1fc678eef2d22626fcf7ad35c7b3475
ft.yazid <mohammadyazidur@gmail.com>
2026-05-23 10:51:07 +0700

    Add model kategori

--- app/Models/kategori.php
+++ app/Models/kategori.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Model;
 
+#[Table('kategoris')]
+#[Fillable(['id', 'nama_kategori', 'deskripsi'])]
 class kategori extends Model
 {
     //
